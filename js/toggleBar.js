2b33d06a9641f637a4a1fb1579a3f70aba635259
Rhuan Vieira <crpvieira6000@gmail.com>
2026-01-06 00:58:09 -0300

    Card contatos 1.0

--- js/toggleBar.js
+++ js/toggleBar.js
@@ -1,7 +1,18 @@
-const toggleBtn = document.querySelector('.toggle-btn');
-const menu = document.querySelector('.menu');
+function toggleModal() {
+    const modal = document.getElementById('contact-modal');
+    const display = modal.style.display;
+    
+    if (display === 'flex') {
+        modal.style.display = 'none';
+    } else {
+        modal.style.display = 'flex';
+    }
+}
 
-toggleBtn.addEventListener('click', () => {
-  toggleBtn.classList.toggle('open');
-  menu.classList.toggle('active'); // mostra/esconde menu
-});
+// Fechar se clicar fora da janela branca
+window.onclick = function(event) {
+    const modal = document.getElementById('contact-modal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
